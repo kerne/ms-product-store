ec1042b4964fc90a67d808c23e54ba3880132ce9
Cesar A <cesar.araya.acosta@gmail.com>
2021-02-12 16:15:39 -0300

    fix import

--- src/test/java/cl/falabella/evaluation/MsProductStoreApplicationTests.java
+++ src/test/java/cl/falabella/evaluation/MsProductStoreApplicationTests.java
@@ -2,7 +2,7 @@ package cl.falabella.evaluation;
 
 import cl.falabella.evaluation.model.ProductUtil;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
