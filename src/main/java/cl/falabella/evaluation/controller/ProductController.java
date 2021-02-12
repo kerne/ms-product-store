e700365729368fe6af0f2ee16e9b947580710abd
Cesar A <cesar.araya.acosta@gmail.com>
2021-02-12 16:26:22 -0300

    fix producers

--- src/main/java/cl/falabella/evaluation/controller/ProductController.java
+++ src/main/java/cl/falabella/evaluation/controller/ProductController.java
@@ -3,6 +3,7 @@ package cl.falabella.evaluation.controller;
 import cl.falabella.evaluation.model.Product;
 import cl.falabella.evaluation.services.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,12 +13,13 @@ public class ProductController {
     @Autowired
     IProductService service;
 
-    @PostMapping("/")
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Object addProducto(@RequestBody Product product) {
         return service.add(product);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+
     public Object getProducto(@PathVariable Integer id) {
         return service.get(id);
     }
