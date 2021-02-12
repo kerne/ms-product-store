42bc682eb216384239a627081c048b00e0799971
Cesar A <cesar.araya.acosta@gmail.com>
2021-02-12 16:13:41 -0300

    fix

--- src/test/java/cl/falabella/evaluation/MsProductStoreApplicationTests.java
+++ src/test/java/cl/falabella/evaluation/MsProductStoreApplicationTests.java
@@ -1,11 +1,8 @@
 package cl.falabella.evaluation;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import cl.falabella.evaluation.model.ProductUtil;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +14,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,6 +25,7 @@ class MsProductStoreApplicationTests {
 
     @Autowired
     WebApplicationContext webApplicationContext;
+
 
     MockMvc mockMvc;
 
