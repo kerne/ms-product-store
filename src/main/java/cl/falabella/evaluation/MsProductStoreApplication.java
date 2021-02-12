83c0740b51ae02067367586f1be4bfb685cf1c0a
Cesar A <cesar.araya.acosta@gmail.com>
2021-02-12 14:45:53 -0300

    initial commit

---
+++ src/main/java/cl/falabella/evaluation/MsProductStoreApplication.java
@@ -0,0 +1,13 @@
+package cl.falabella.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsProductStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/falabella/evaluation/MsProductStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.falabella.evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
